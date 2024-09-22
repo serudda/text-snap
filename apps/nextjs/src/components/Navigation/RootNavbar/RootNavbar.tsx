7e103761c84e023f3920aaf88ed21b1263d06650
Samuel Llibre-Pillco <dev@zyruks.com>
2024-09-22 13:01:41 -0400

    feat(Navigations): ✨ add SettingsModal to RootNavbar
    - Integrated SettingsModal into RootNavbar for handling settings
    - Added modal functionality with useModal hook and openModal logic

--- apps/nextjs/src/components/Navigation/RootNavbar/RootNavbar.tsx
+++ apps/nextjs/src/components/Navigation/RootNavbar/RootNavbar.tsx
@@ -1,4 +1,5 @@
-import { Button, ButtonVariant, cn, Icon, IconCatalog } from 'side-ui';
+import { SettingsModal } from '~/components/Modals';
+import { Button, ButtonVariant, cn, Icon, IconCatalog, useModal } from 'side-ui';
 
 interface RootNavbarProps {
   /**
@@ -12,21 +13,32 @@ export const RootNavbar = ({ className }: RootNavbarProps) => {
     container: cn('py-4', className),
   };
 
+  const { modalNode, openModal } = useModal();
+
+  const handleOpenModal = () => {
+    return openModal<boolean | null>((close) => <SettingsModal onClose={close} />);
+  };
+
   return (
-    <header className={classes.container}>
-      <div className="container mx-auto">
-        <nav className="flex justify-end">
-          <Button
-            variant={ButtonVariant.ghost}
-            className="group aspect-square rounded-full transition-colors duration-200 ease-in-out"
-          >
-            <Icon
-              icon={IconCatalog.cog6Tooth}
-              className="min-h-6 min-w-6 text-white transition-colors duration-200 ease-in-out group-hover:text-black"
-            />
-          </Button>
-        </nav>
-      </div>
-    </header>
+    <>
+      <header className={classes.container}>
+        <div className="container mx-auto">
+          <nav className="flex justify-end">
+            <Button
+              onClick={() => void handleOpenModal()}
+              variant={ButtonVariant.ghost}
+              className="dark:transparent group aspect-square rounded-full transition-colors duration-200 ease-in-out dark:hover:bg-white"
+            >
+              <Icon
+                icon={IconCatalog.cog6Tooth}
+                className="min-h-6 min-w-6 text-white transition-colors duration-200 ease-in-out group-hover:text-black"
+              />
+            </Button>
+          </nav>
+        </div>
+      </header>
+
+      {modalNode}
+    </>
   );
 };
