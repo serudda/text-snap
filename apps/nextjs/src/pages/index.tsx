3fc6b5772555e063ad16220c9a05d2ed33130d5a
Samuel Llibre-Pillco <dev@zyruks.com>
2024-09-22 11:20:33 -0400

    refactor(pages): ♻️ update Home page layout and content
    - Replaced the existing layout with RootLayout
    - Added text area component for user input with state management

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -1,66 +1,32 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Link from 'next/link';
-import { Icon, IconCatalog } from '~/components';
-import { signIn, useSession } from 'next-auth/react';
-import { Button, ButtonSize, ButtonVariant } from 'side-ui';
+import { useState, type ChangeEvent, type ReactElement } from 'react';
+import { type NextPageWithLayout } from './_app';
+import { RootLayout } from '~layout';
+import { Resize, Textarea } from 'side-ui';
 
-const Home: NextPage = () => {
-  const { data: sessionData } = useSession();
+const Home: NextPageWithLayout = () => {
+  const [text, setText] = useState('');
+
+  const handleTextChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setText(event.target.value);
+  };
 
   return (
-    <>
-      <Head>
-        <title>Indie Creators HQ - Side Project Starter Kit</title>
-        <meta name="description" content="Side Project Starter Kit" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="relative flex h-screen w-full flex-none flex-col items-center justify-center gap-10 overflow-hidden bg-slate-900 p-10">
-        <div className="relative flex h-min w-min flex-none flex-col flex-wrap items-center justify-center gap-4 overflow-hidden p-0">
-          <div className="mb-2 flex flex-row items-center gap-4 text-white">
-            <a className="text-2xl font-semibold text-secondary-300 transition hover:opacity-80" href="/">
-              Side Project Starter Kit
-            </a>
-          </div>
-          <div className="mb-4 w-96">
-            <h1 className="text-center text-3xl font-bold text-slate-50">This is a starter kit template</h1>
-          </div>
-          {sessionData ? (
-            <p className="text-lg text-white">{sessionData.user.name}</p>
-          ) : (
-            <Button variant={ButtonVariant.secondary} size={ButtonSize.sm} onClick={() => void signIn('twitter')}>
-              Log in with Twitter
-            </Button>
-          )}
-        </div>
-
-        {/* Footer options */}
-        <div className="flex flex-wrap justify-center gap-x-1 gap-y-3 sm:gap-x-2 lg:justify-start">
-          <Link
-            className="group relative isolate flex flex-none items-center gap-x-3 rounded-lg px-2 py-0.5 font-medium text-white/30 transition-colors hover:text-primary-200"
-            href="https://indiecreatorshq.com/discord"
-            target="_blank"
-          >
-            <Icon icon={IconCatalog.discord} className="h-6 w-6 text-white" isSolid />
-            <span className="self-baseline text-white">Discord</span>
-          </Link>
-          <div className="mx-2 h-[30px] w-[0.5px] rotate-[20deg] transform bg-neutral-700"></div>
-          <div className="flex items-center gap-x-1">
-            <span className="text-slate-400">Made with</span>
-            <Icon icon={IconCatalog.heart} className="h-4 w-4 text-red-500" isSolid />
-            <span className="text-slate-400">by the</span>
-            <Link
-              className="font-medium text-slate-400 underline decoration-dashed decoration-0 underline-offset-4 transition-colors hover:text-primary-200"
-              href="https://github.com/Indie-Creator-Community"
-              target="_blank"
-            >
-              Indie Creators HQ
-            </Link>
-          </div>
-        </div>
-      </main>
-    </>
+    <main>
+      <div className="container mx-auto p-4">
+        <Textarea
+          className="bg-neutral-950 text-white"
+          textareaClassName="placeholder:text-gray-400 hover:placeholder:text-gray-200 focus:placeholder:text-gray-200"
+          placeholder="Start your journey..."
+          styleless
+          hasAutoSize
+          resize={Resize.none}
+          value={text}
+          onChange={handleTextChange}
+        />
+      </div>
+    </main>
   );
 };
 
+Home.getLayout = (page: ReactElement) => <RootLayout>{page}</RootLayout>;
 export default Home;
