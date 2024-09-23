ca048f4c01a6821854619a58d32cc56a8a8c7513
serudda <sergioruizdavila@gmail.com>
2024-09-22 20:57:24 -0500

    refactor: change dispatchFormat to mutation

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -20,42 +20,30 @@ import {
 const Home: NextPageWithLayout = () => {
   const [text, setText] = useState('');
   const { shortcuts } = useHotkeySettings();
-  const [selectedFormat, setSelectedFormat] = useState(Format.grammar);
 
-  const { refetch: dispatchFormat, isFetching } = api.ai.dispatchFormat.useQuery(
-    {
-      text,
-      selectedFormat: {
-        type: selectedFormat,
-      },
+  const { mutate: dispatchFormat, isLoading } = api.ai.dispatchFormat.useMutation({
+    retry: false,
+    cacheTime: 0,
+    onSuccess(response) {
+      if (!response?.data) return;
+      setText(response.data.formattedText);
     },
-    {
-      enabled: false,
-      retry: false,
-      cacheTime: 0,
-      onSuccess() {
-        console.log('dispatchFormat - onSuccess');
-      },
-      onError(error) {
-        console.error('dispatchFormat - onError', error);
-      },
+    onError(error) {
+      console.error('dispatchFormat - onError', error);
     },
-  );
+  });
 
   const handleTextChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setText(event.target.value);
   };
 
-  const handleHotKey = async (formatType: Format) => {
-    setSelectedFormat(formatType);
-    const response = await dispatchFormat();
-
-    if (!response.data?.data) {
-      console.error('Error fetching data');
-      return;
-    }
-
-    setText(response.data.data.formattedText);
+  const handleHotKey = (formatType: Format) => {
+    dispatchFormat({
+      text,
+      selectedFormat: {
+        type: formatType,
+      },
+    });
   };
 
   useHotkeys(shortcuts.translate, (event) => {
@@ -91,9 +79,9 @@ const Home: NextPageWithLayout = () => {
   return (
     <main>
       <div className="mx-auto max-w-4xl p-4 px-11">
-        {isFetching && <span className="animate-pulse text-primary-200">formateando...</span>}
+        {isLoading && <span className="animate-pulse text-primary-200">formateando...</span>}
         <div className="mb-3 flex w-full justify-between border-b border-neutral-900 px-2 pb-4">
-          <div className="flex items-center gap-1">
+          <div className="flex hidden items-center gap-1">
             <Button variant={ButtonVariant.ghost} size={ButtonSize.xs}>
               <Icon icon={IconCatalog.chevronRight} iconStyle={IconStyle.regular} className="h-4 w-4 rotate-180" />
             </Button>
@@ -102,7 +90,7 @@ const Home: NextPageWithLayout = () => {
               <Icon icon={IconCatalog.chevronRight} iconStyle={IconStyle.regular} className="h-4 w-4" />
             </Button>
           </div>
-          <div className="flex items-center gap-3">
+          <div className="ml-auto flex items-center gap-3">
             <Button className="flex gap-2" variant={ButtonVariant.tertiary} size={ButtonSize.sm}>
               <Icon icon={IconCatalog.clipboard} iconStyle={IconStyle.regular} className="h-4 w-4" />
               <span>Copy</span>
@@ -124,7 +112,7 @@ const Home: NextPageWithLayout = () => {
           value={text}
           onChange={handleTextChange}
         />
-        <div className="flex items-center gap-2 p-4">
+        <div className="flex hidden items-center gap-2 p-4">
           <span className="text-sm text-neutral-500">Format applied:</span>
           <Tag variant={TagVariant.success}>Emoji</Tag>
         </div>
