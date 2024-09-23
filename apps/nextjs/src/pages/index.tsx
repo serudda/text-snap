6d0b26e249ce4d833d1350d1163a7d2e585d8a76
serudda <sergioruizdavila@gmail.com>
2024-09-22 19:49:10 -0500

    refactor: improve page style composition

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -4,7 +4,18 @@ import { useHotkeySettings } from '~/common';
 import { type NextPageWithLayout } from './_app';
 import { RootLayout } from '~layout';
 import { useHotkeys } from 'react-hotkeys-hook';
-import { Resize, Textarea } from 'side-ui';
+import {
+  Button,
+  ButtonSize,
+  ButtonVariant,
+  Icon,
+  IconCatalog,
+  IconStyle,
+  Resize,
+  Tag,
+  TagVariant,
+  Textarea,
+} from 'side-ui';
 
 const Home: NextPageWithLayout = () => {
   const [text, setText] = useState('');
@@ -79,18 +90,44 @@ const Home: NextPageWithLayout = () => {
 
   return (
     <main>
-      <div className="container mx-auto p-4">
+      <div className="mx-auto max-w-4xl p-4 px-11">
         {isFetching && <span className="animate-pulse text-primary-200">formateando...</span>}
+        <div className="mb-3 flex w-full justify-between border-b border-neutral-900 px-2 pb-4">
+          <div className="flex items-center gap-1">
+            <Button variant={ButtonVariant.ghost} size={ButtonSize.xs}>
+              <Icon icon={IconCatalog.chevronRight} iconStyle={IconStyle.regular} className="h-4 w-4 rotate-180" />
+            </Button>
+            <span className="text-white">3/3</span>
+            <Button variant={ButtonVariant.ghost} size={ButtonSize.xs}>
+              <Icon icon={IconCatalog.chevronRight} iconStyle={IconStyle.regular} className="h-4 w-4" />
+            </Button>
+          </div>
+          <div className="flex items-center gap-3">
+            <Button className="flex gap-2" variant={ButtonVariant.tertiary} size={ButtonSize.sm}>
+              <Icon icon={IconCatalog.clipboard} iconStyle={IconStyle.regular} className="h-4 w-4" />
+              <span>Copy</span>
+            </Button>
+            <Button className="flex gap-2" variant={ButtonVariant.tertiary} size={ButtonSize.sm}>
+              <Icon icon={IconCatalog.trash} iconStyle={IconStyle.regular} className="h-4 w-4" />
+              <span>Clean</span>
+            </Button>
+          </div>
+        </div>
         <Textarea
-          className="bg-neutral-950 text-white"
-          textareaClassName="placeholder:text-gray-400 hover:placeholder:text-gray-200 focus:placeholder:text-gray-200"
+          className="border-b border-neutral-900 bg-neutral-950 p-4 text-white"
+          textareaClassName="placeholder:text-neutral-600"
           placeholder="Start your journey..."
           styleless
           hasAutoSize
+          minHeight="30vh"
           resize={Resize.none}
           value={text}
           onChange={handleTextChange}
         />
+        <div className="flex items-center gap-2 p-4">
+          <span className="text-sm text-neutral-500">Format applied:</span>
+          <Tag variant={TagVariant.success}>Emoji</Tag>
+        </div>
       </div>
     </main>
   );
