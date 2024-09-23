8fe9aa052ac93726cd996beee06196bbda709608
Samuel Llibre-Pillco <dev@zyruks.com>
2024-09-23 00:52:38 -0400

    feat(layout): ✨ add footer with credits and keyboard shortcuts to RootLayout

--- apps/nextjs/src/layout/BaseLayout/BaseLayout.tsx
+++ apps/nextjs/src/layout/BaseLayout/BaseLayout.tsx
@@ -1,5 +1,4 @@
 import { type ReactNode } from 'react';
-import { RootNavbar } from '~components';
 
 export interface RootLayoutProps {
   /**
@@ -10,10 +9,25 @@ export interface RootLayoutProps {
 
 export const RootLayout = ({ children }: RootLayoutProps) => {
   return (
-    <div className="grid h-dvh grid-rows-[auto,1fr] bg-neutral-950">
-      <RootNavbar />
-
+    <div className="grid h-dvh grid-rows-[1fr,auto] bg-neutral-950">
       {children}
+      <footer className="grid place-items-center gap-4">
+        <div className="flex select-none items-center gap-2 text-neutral-500">
+          The best version of your words
+          <kbd className="flex aspect-square items-center rounded-md bg-neutral-900 p-2 text-xs">⌘</kbd>
+          <kbd className="flex aspect-square items-center rounded-md bg-neutral-900 p-2 text-xs">K</kbd>
+        </div>
+        <p className="mb-6 text-center text-lg font-semibold text-neutral-200">
+          With 💘 from&nbsp;
+          <a href="https://x.com/serudda" className="text-primary-300 transition-colors hover:text-primary-200">
+            @serudda
+          </a>
+          &nbsp;and&nbsp;
+          <a href="https://x.com/zyruks" className="text-primary-300 transition-colors hover:text-primary-200">
+            @zyruks
+          </a>
+        </p>
+      </footer>
     </div>
   );
 };
