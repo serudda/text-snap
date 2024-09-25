780ee85d8e351ea46796b228fe3b7c2983955697
Samuel Llibre-Pillco <dev@zyruks.com>
2024-09-25 14:14:04 -0400

    refactor: 🔥 move to  utils

--- apps/nextjs/src/common/constants/hotkeys.ts
+++
@@ -1,6 +0,0 @@
-import { getOS } from '../utils';
-import { OperatingSystem } from './general';
-
-const currentOs = getOS(navigator.userAgent);
-
-export const primaryHotkey = currentOs === OperatingSystem.windows ? 'CTRL' : '⌘';
--- apps/nextjs/src/common/constants/index.ts
+++ apps/nextjs/src/common/constants/index.ts
@@ -1,5 +1,4 @@
 export * from './errors';
 export * from './general';
-export * from './hotkeys';
 export * from './inputs';
 export * from './localStorage';
